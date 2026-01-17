a6fe7837f932d412e9d313bdd6278edbba14642c
Deniz Isikli <s215818@dtu.dk>
2026-01-17 23:59:26 +0100

    ExecuteAsynchronousFunctionsinParallel.ts

---
+++ Problemsets/ExecuteAsynchronousFunctionsinParallel.ts
@@ -0,0 +1,18 @@
+async function promiseAll<T>(functions: (() => Promise<T>)[]): Promise<T[]> {
+  return new Promise((resolve, reject) => {
+    const result = new Array(functions.length);
+    let count = functions.length;
+
+    for (let index = 0; index < functions.length; index++) {
+      const func = functions[index];
+      func()
+        .then((value) => {
+          result[index] = value;
+          count--;
+
+          if (count === 0) return resolve(result);
+        })
+        .catch(reject);
+    }
+  })
+};
